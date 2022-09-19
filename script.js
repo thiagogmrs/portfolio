870ff7af1bcaf90b9fa8beeef581d4ad689f34fa
Thiago Guimarães <104789081+thiagogmrs@users.noreply.github.com>
2022-09-19 19:11:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,39 @@ btn.addEventListener('click', ()=>{
 
 });
 
+
+document.querySelector('#qtde-pages').addEventListener('change', updateValue)
+document.querySelector('#js').addEventListener('change', updateValue)
+document.querySelector('#layout-sim').addEventListener('change', updateValue)
+document.querySelector('#layout-nao').addEventListener('change', updateValue)
+document.querySelector('#prazo').addEventListener('change', ()=> {
+     const prazo = document.querySelector('#prazo').value
+     document.querySelector('label[for=prazo]').innerHTML = `Prazo ${prazo} Semanas`
+     updateValue()
+});
+
+function updateValue(){
+    const qtdePages = document.querySelector('#qtde-pages').value
+    const needJS = document.querySelector('#js').checked
+    const needLayout = document.querySelector('#layout-sim').checked
+    const prazo = document.querySelector('#prazo').value
+    let price = qtdePages * 100
+    if(needJS){
+        price = price + (price * 50/100)
+    }
+    if(needLayout){
+        price = price + 500
+    }
+    let urgencyRate = 1 - prazo*0.1;
+    price *= 1 + urgencyRate
+
+
+    document.querySelector('#preco').innerHTML = `R$ ${price.toFixed(2)}`
+}
+
+
+
+
 //back-to-top
 
 // const backToTop = document.getElementById('back-to-top');
